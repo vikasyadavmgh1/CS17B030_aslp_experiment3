bc2f51e9cabde5d16c415d334772ac43ad0bff45
vikasyadavmgh1 <46579571+vikasyadavmgh1@users.noreply.github.com>
2019-02-22 17:42:54 +0530

    Update lab3_2_s.s

--- lab3_2_s.s
+++ lab3_2_s.s
@@ -1,11 +1,16 @@
 main:
-addi $sp,$sp,-12
-sw $t0,0($sp)
-sw $s0,4($sp)
-li $t0,34
-li $s0,45
-add $t0,$s0,$t0
-sub $s0,$t0,$s0
-sub $t0,$t0,$s0
-addi $sp,$sp,12
+	addi $s0, 12 
+	addi $s1, 15 
+	addi $s2, 23 
+	addi $s3, 41 
 
+	mul $t0, $s3, $s3
+	mul $t0, $t0, $s0
+	mul $t1, $s1, $s3
+	add $t2, $t0, $s2
+	sub $s4, $t2, $t1
+
+	li $v0, 10
+	syscall
+
+.end main
